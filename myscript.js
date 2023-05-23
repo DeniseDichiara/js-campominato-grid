2eace8789308ef12c69d27f385e9fae863c5ecb0
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-23 17:14:53 +0200

    created grid

--- myscript.js
+++ myscript.js
@@ -3,7 +3,7 @@ let tagName
 
 let className
 
-function createCell(tagName, className){
+function createElement(tagName, className){
     const cellElement = document.createElement (tagName);
     cellElement.className += className;
     return cellElement;
@@ -15,5 +15,11 @@ const gridElement = document.getElementById('grid');
 for (let i = 0; i < 100; i ++){
 
     const actualCell = createElement('div', 'cell');
-    gridElement.appendChild(createElement ('div' , 'cell'));
+
+    actualCell.addEventListener('click', function(){
+        
+        actualCell.classList.toggle('selected')
+    })
+
+    gridElement.appendChild(actualCell);
 }
