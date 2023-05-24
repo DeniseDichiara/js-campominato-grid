d030d7c589011bf93b4368555373a33523caf9b0
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-24 11:45:02 +0200

    for

---
+++ js-snack-random/myscript.js
@@ -0,0 +1,10 @@
+
+const arrayNumbers = [1,2,3,4,5];
+
+for(let i = 0; i < arrayNumbers.length; i++){
+
+    //if(arrayNumbers[i] !== arrayNumbers[i]){
+        console.log(arrayNumbers[i]);
+    }
+    
+//}
