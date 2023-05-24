5381b5c7372adfa1d6bb6780893c57dc311c61e1
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-24 09:12:33 +0200

    created li element

--- myscript.js
+++ myscript.js
@@ -7,14 +7,26 @@ function createElement(tagName, className){
     const cellElement = document.createElement (tagName);
     cellElement.className += className;
     return cellElement;
-
 }
 
+function getNewLiElement (){
+    const newLiElement = document.createElement('li');
+    newLiElement.classList.add('list-item');
+    return newLiElement;
+}
+
+
 const gridElement = document.getElementById('grid');
+
+const olElement = document.querySelector('ol');
 
 for (let i = 0; i < 100; i ++){
 
     const actualCell = createElement('div', 'cell');
+
+    for(let i = 1; i <= 100; i++){
+        olElement.appendChild(getNewLiElement());
+    }
 
     actualCell.addEventListener('click', function(){
         
@@ -23,3 +35,4 @@ for (let i = 0; i < 100; i ++){
 
     gridElement.appendChild(actualCell);
 }
+
